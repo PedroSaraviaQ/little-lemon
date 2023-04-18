d60f9f9f8f4e7e338d9392ac20643943265019c4
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-04-18 14:10:35 -0500

    basic footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <img src="" alt="" />
+      <div className="links">
+        <div className="navigation">
+          <h3>
+            Doormat
+            <br />
+            Navigation
+          </h3>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className="contact">
+          <h3>Contact</h3>
+          <ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </div>
+        <div className="media">
+          <h3>Social Media Links</h3>
+          <ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
