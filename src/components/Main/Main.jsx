0b5625995e346cff9c09491e3d97dd4d9a1d7799
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-04-21 16:34:54 -0500

    class 'principal' to 'banner'

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,17 @@
 import Banner from "../../assets/images/banner.jpg";
 import Button from "../Button/Button";
-import "./Main.css";
+import "./banner.css";
 
 function Main() {
   return (
     <main>
-      <section className="principal">
-        <div>
+      <section className="banner">
+        <article>
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
           <Button>Reserve a Table</Button>
-        </div>
+        </article>
         <img src={Banner} alt="Little Lemon Banner" />
       </section>
     </main>
