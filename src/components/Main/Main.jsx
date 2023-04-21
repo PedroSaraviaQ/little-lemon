6045b04a2847f5f4bc7ab5cf46c544280d0ac95c
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-04-21 16:07:08 -0500

    xxl size main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../assets/images/banner.jpg";
+import Button from "../Button/Button";
 import "./Main.css";
 
 function Main() {
@@ -9,9 +10,9 @@ function Main() {
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-          <button>Reserve a Table</button>
+          <Button>Reserve a Table</Button>
         </div>
-        <img src={Banner} alt="" />
+        <img src={Banner} alt="Little Lemon Banner" />
       </section>
     </main>
   );
