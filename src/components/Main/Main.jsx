27678b8af0286b272b476ea2a46673cc2885be15
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-04-21 17:23:39 -0500

    md-lg size banner

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,9 +10,9 @@ function Main() {
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-          <Button>Reserve a Table</Button>
         </article>
         <img src={Banner} alt="Little Lemon Banner" />
+        <Button>Reserve a Table</Button>
       </section>
     </main>
   );
